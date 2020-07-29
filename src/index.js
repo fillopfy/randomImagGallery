4b1af22f748e9d07087bfa2263f31df832278d79
fillopfy <abhi456tm@gmail.com>
2020-07-29 08:01:20 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+
+// const name = "Abhishek Tiwari";
+const date=new Date().toDateString();
+const time =new Date().toLocaleTimeString();
+const img1="https://picsum.photos/200/300";
+const img2="https://picsum.photos/250/300";
+const img3="https://picsum.photos/300/300";
+
+const img4="https://picsum.photos/270/300";
+const img5="https://picsum.photos/290/300";
+const img6="https://picsum.photos/320/300";
+
+const img7="https://picsum.photos/310/300";
+const img8="https://picsum.photos/350/300";
+const img9="https://picsum.photos/370/300";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <header>
+      <h1 className="header">{`My Random Image Gallery`}</h1>
+      <br/>
+      <p>Current date: {date}</p>
+      <br/>
+      <p>Current time: {time}</p>
+    </header>
+    <br/>
+    <div className="img">
+      <div className="row">
+        <img src={img1} alt="kuch bhi" />
+        <img src={img2} alt="kuch bhi" />
+        <img src={img3} alt="kuch bhi" />
+      </div>
+      <div className="row">
+        <img src={img4} alt="kuch bhi" />
+        <img src={img5} alt="kuch bhi" />
+        <img src={img6} alt="kuch bhi" />
+      </div>
+      <div className="row">
+        <img src={img7} alt="kuch bhi" />
+        <img src={img8} alt="kuch bhi" />
+        <img src={img9} alt="kuch bhi" />
+      </div>
+    </div>
+    <br/>
+    <footer>
+      <h2>Copyright &copy; fillopfy</h2>
+    </footer>
+
+  </div>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
